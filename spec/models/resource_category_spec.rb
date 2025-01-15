1e218ca6c1905767b3b1b7c353ad27c610e347c1
Keenan Bromley <bromleyk@oregonstate.edu>
2025-01-15 14:47:21 -0800

    Added all responds to tests for the resource_category_spec

--- spec/models/resource_category_spec.rb
+++ spec/models/resource_category_spec.rb
@@ -2,8 +2,20 @@ require 'rails_helper'
 
 RSpec.describe ResourceCategory, type: :model do
 
+  let (:res_cat) {ResourceCategory.new}
+  
   it "exists" do
     ResourceCategory.new
   end
+
+  it "responds to name" do
+    expect(res_cat).to respond_to(:name)
+  end 
+
+  it "responds to active" do
+    expect(res_cat).to respond_to(:active)
+  end
+
+
 
 end
