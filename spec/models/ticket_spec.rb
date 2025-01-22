7e400abd347b3ff0977368b0d18e555a79aa5b28
Colin Mant <colinm6666@gmail.com>
2025-01-22 14:16:11 -0800

    Added validates tests to ticket_spec.rb

--- spec/models/ticket_spec.rb
+++ spec/models/ticket_spec.rb
@@ -7,38 +7,39 @@ RSpec.describe Ticket, type: :model do
   it "exists" do
     Ticket.new
   end
+  describe "responds to" do
+    it "responds to name" do 
+      expect(ticket).to respond_to(:name)
+    end 
 
-  it "responds to name" do 
-    expect(ticket).to respond_to(:name)
-  end 
+    it "responds to description" do
+      expect(ticket).to respond_to(:description)
+    end 
 
-  it "responds to description" do
-    expect(ticket).to respond_to(:description)
-  end 
+    it "responds to phone" do
+      expect(ticket).to respond_to(:phone)
+    end 
 
-  it "responds to phone" do
-    expect(ticket).to respond_to(:phone)
-  end 
+    it "responds to organization_id" do
+      expect(ticket).to respond_to(:organization_id)
+    end 
 
-  it "responds to organization_id" do
-    expect(ticket).to respond_to(:organization_id)
-  end 
+    it "responds to closed" do
+      expect(ticket).to respond_to(:closed)
+    end 
 
-  it "responds to closed" do
-    expect(ticket).to respond_to(:closed)
-  end 
+    it "responds to closed_at" do
+      expect(ticket).to respond_to(:closed_at)
+    end 
 
-  it "responds to closed_at" do
-    expect(ticket).to respond_to(:closed_at)
-  end 
+    it "responds to resource_category_id" do
+      expect(ticket).to respond_to(:resource_category_id)
+    end 
 
-  it "responds to resource_category_id" do
-    expect(ticket).to respond_to(:resource_category_id)
-  end 
-
-  it "responds to region_id" do
-    expect(ticket).to respond_to(:region_id)
-  end 
+    it "responds to region_id" do
+      expect(ticket).to respond_to(:region_id)
+    end 
+  end
 
   it "belongs to organization" do 
     belong_to(:organization)
@@ -50,5 +51,34 @@ RSpec.describe Ticket, type: :model do
   
   it "belongs to resource_category" do
     belong_to(:resource_category)
+  end
+  describe "validation tests" do
+    it "validates presence of name" do
+      should validate_presence_of(:name)
+    end
+
+    it "validates presence of phone" do
+      should validate_presence_of(:phone)
+    end
+
+    it "validates presence of region_id" do
+      should validate_presence_of(:region_id)
+    end
+
+    it "validates presence of resource_category_id" do
+      should validate_presence_of(:resource_category_id)
+    end
+
+    it "validates length of name" do
+      should validate_length_of(:name).is_at_least(1).is_at_most(255).on(:create)
+    end
+
+    it "validates length of description" do
+      should validate_length_of(:description).is_at_most(1020).on(:create)
+    end
+
+    it "validates phone" do
+      should validate(:phone).phony_plausible
+    end
   end
 end
