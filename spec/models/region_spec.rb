6e3d29361fda7db09b3a4debfbd93bfa4d5eeed5
Keenan Bromley <bromleyk@oregonstate.edu>
2025-01-27 12:55:26 -0800

    Updated validation tests

--- spec/models/region_spec.rb
+++ spec/models/region_spec.rb
@@ -26,7 +26,7 @@ RSpec.describe Region, type: :model do
     end
 
     it "validates length of name" do
-      should validate_length_of(:name)
+      should validate_length_of(:name).is_at_least(1).is_at_most(255).on(:create)
     end
 
     it "validates uniqueness of name" do
