25b9200ef981d6d55e8af0efc699391b204f3157
Keenan Bromley <bromleyk@oregonstate.edu>
2025-01-27 12:27:58 -0800

    Added 'validation tests'

--- spec/models/region_spec.rb
+++ spec/models/region_spec.rb
@@ -20,4 +20,20 @@ RSpec.describe Region, type: :model do
     should have_many(:tickets)
   end
 
+  describe "validation tests" do
+    it "validates presence of name" do
+      should validate_presence_of(:name)
+    end
+
+    it "validates length of name" do
+      should validate_length_of(:name)
+    end
+
+    it "validates uniqueness of name" do
+      should validate_uniqueness_of(:name).case_insensitive
+    end 
+  end
+
+  
+
 end
