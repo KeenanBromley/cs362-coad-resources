9e84ff2d1762d31da9fcc97cee73aa19d09c7279
Keenan Bromley <bromleyk@oregonstate.edu>
2025-01-27 12:41:18 -0800

    Added validation tests

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -20,4 +20,26 @@ RSpec.describe User, type: :model do
     belong_to(:organization)
   end
 
+  describe "validation tests" do
+    it "validates presence of email" do
+      should validate_presence_of(:email)
+    end 
+
+    it "validates length of email" do
+      should validate_length_of(:email).is_at_least(1).is_at_most(255).on(:create)
+    end 
+
+    it "validates uniqueness of email" do
+      should validate_uniqueness_of(:email).case_insensitive
+    end
+    
+    it "validates presence of password" do
+      should validate_presence_of(:password).on(:create)
+    end
+
+    it "validates length of password" do
+      should validate_length_of(:password).is_at_least(7).is_at_most(255).on(:create)
+    end 
+  end 
+
 end
