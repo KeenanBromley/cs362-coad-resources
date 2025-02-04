fedc574fc72f8237bd43afa0c907b6ab787bc2b5
Keenan Bromley <bromleyk@oregonstate.edu>
2025-02-03 18:14:00 -0800

    Created tests for dashboard_helper_spec.rb

--- spec/helpers/dashboard_helper_spec.rb
+++ spec/helpers/dashboard_helper_spec.rb
@@ -11,5 +11,51 @@ require 'rails_helper'
 #   end
 # end
 RSpec.describe DashboardHelper, type: :helper do
+  describe "#dashboard_for" do
+    let(:user) {double('User')}
 
+    context "When user is admin" do
+      it "returns 'admin_dashboard'" do
+        allow(user).to receive(:admin?).and_return(true)
+        expect(helper.dashboard_for(user)).to eq('admin_dashboard')
+      end
+    end
+
+    context "When user has an organization that has submitted" do
+      it "returns 'organization_submitted_dashboard'" do
+        organization = double ('Organization')
+        allow(user).to receive(:admin?).and_return(false)
+        allow(user).to receive(:organization).and_return(organization)
+        allow(organization).to receive(:submitted?).and_return(true)
+        allow(organization).to receive(:approved?).and_return(false)
+
+        expect(helper.dashboard_for(user)).to eq('organization_submitted_dashboard')
+      end
+    end
+
+    context "When user has an organization that is approved" do
+      it "returns 'organization_approved_dashboard'" do
+        organization = double('Organization')
+        allow(user).to receive(:admin?).and_return(false)
+        allow(user).to receive(:organization).and_return(organization)
+        allow(organization).to receive(:submitted?).and_return(false)
+        allow(organization).to receive(:approved?).and_return(true)
+
+        expect(helper.dashboard_for(user)).to eq('organization_approved_dashboard')
+      end
+    end
+
+    context "When user has no organization or its status does not meet any condtion" do
+      it "returns 'create_application_dashboard'" do
+        organization = double('Organization')
+        allow(user).to receive(:admin?).and_return(false)
+        allow(user).to receive(:organization).and_return(organization)
+        allow(organization).to receive(:submitted?).and_return(false)
+        allow(organization).to receive(:approved?).and_return(false)
+
+        expect(helper.dashboard_for(user)).to eq('create_application_dashboard')
+      end
+    end
+
+  end  
 end
