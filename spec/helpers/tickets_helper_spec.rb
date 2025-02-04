333d3f752d6630dabeb4129bbf7e0dd98d6771ec
Keenan Bromley <bromleyk@oregonstate.edu>
2025-02-03 18:29:55 -0800

    Created tests for tickets_helper_spec.rb

--- spec/helpers/tickets_helper_spec.rb
+++ spec/helpers/tickets_helper_spec.rb
@@ -11,5 +11,29 @@ require 'rails_helper'
 #   end
 # end
 RSpec.describe TicketsHelper, type: :helper do
+  describe "#format_phone_number" do
+    it "normalizes a standard US phone number" do
+      expect(helper.format_phone_number("555-123-4567")).to eq("+15551234567")
+    end
 
+    it "normalizes a US phone number with parentheses and spaces" do
+      expect(helper.format_phone_number("(555) 123-4567")).to eq("+15551234567")
+    end
+
+    it "normalizes a US phone number with country code" do
+      expect(helper.format_phone_number("1-555-123-4567")).to eq("+15551234567")
+    end
+
+    it "returns nil for an empty phone number" do
+      expect(helper.format_phone_number("")).to be_nil
+    end
+
+    it "returns nil for nil input" do
+      expect(helper.format_phone_number(nil)).to be_nil
+    end
+
+    it "does not modify an already formatted international number" do
+      expect(helper.format_phone_number("+15551234567")).to eq("+15551234567")
+    end
+  end
 end
