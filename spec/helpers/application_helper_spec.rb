fb07ec04bbd34735f56e56546d6c4ce765f502bc
Keenan Bromley <bromleyk@oregonstate.edu>
2025-02-03 18:22:29 -0800

    Created tests for application_helper_spec.rb

--- spec/helpers/application_helper_spec.rb
+++ spec/helpers/application_helper_spec.rb
@@ -1,5 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe ApplicationHelper, type: :helper do
+    describe "#full title" do
+      it "returns only the base title when page_title is empty" do
+        expect(helper.full_title('')).to eq('Disaster Resource Network')
+      end
+
+      it "returns page title followed by base title when page_title is provided" do
+        expect(helper.full_title('Home')).to eq('Home | Disaster Resource Network')
+      end
+
+      it "returns page title followed by base title when page_title has multiple words" do
+        expect(helper.full_title('About Us')).to eq('About Us | Disaster Resource Network')
+      end
+    end
 
 end
