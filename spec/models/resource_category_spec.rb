4cc8d4b5f6bf1e992952246535208196fe3eb830
Keenan Bromley <bromleyk@oregonstate.edu>
2025-02-10 12:59:05 -0800

    Updated resource_categories tests

--- spec/models/resource_category_spec.rb
+++ spec/models/resource_category_spec.rb
@@ -1,71 +1,54 @@
 require 'rails_helper'
 
 RSpec.describe ResourceCategory, type: :model do
-
-  let (:res_cat) {ResourceCategory.new}
+  let(:res_cat) { FactoryBot.build_stubbed(:resource_category) }
   
   it "exists" do
-    ResourceCategory.new
+    expect(build(:resource_category)).to be_valid
   end
 
-  describe "responds to" do
-    it "responds to name" do
-      expect(res_cat).to respond_to(:name)
-    end 
-
-    it "responds to active" do
-      expect(res_cat).to respond_to(:active)
-    end
-
-    it "has many tickets" do
-      should have_many(:tickets)
-    end
-    
-    it "has and belongs to many organizations" do
-      should have_and_belong_to_many(:organizations)
-    end
+  describe "associations" do
+    it { should have_many(:tickets) }
+    it { should have_and_belong_to_many(:organizations) }
   end
 
-  describe "validation tests" do
-    it "validates presence of name" do
-      should validate_presence_of(:name)
-    end
-
-    it "validates length of name" do
-      should validate_length_of(:name).is_at_least(1).is_at_most(255).on(:create)
-    end
-
-    it "validates uniqueness of name" do
-      should validate_uniqueness_of(:name).case_insensitive
-    end
+  describe "validations" do
+    it { should validate_presence_of(:name) }
+    it { should validate_length_of(:name).is_at_least(1).is_at_most(255).on(:create) }
+    it { should validate_uniqueness_of(:name).case_insensitive }
   end
 
-  describe "member function tests" do
-    it "activates" do
-      res_cat.activate
-      expect(res_cat.active).to eq(true)
+  
+
+  describe "instance methods" do
+    it "#activate" do
+      res_cat1 = create(:resource_category)
+      res_cat1.activate
+      expect(res_cat1.active).to eq(true)
     end
 
-    it "deactivates" do
-      res_cat.deactivate
-      expect(res_cat.active).to eq(false)
+    it "#deactivate" do
+      res_cat1 = create(:resource_category)
+      res_cat1.deactivate
+      expect(res_cat1.active).to eq(false)
     end
 
-    it "is inactive?" do
+    it "#inactive?" do
       expect(res_cat.inactive?).to eq(!res_cat.active)
     end
 
-    it "responds to to_s" do
+    it "#to_s" do
       expect(res_cat.to_s).to eq(res_cat.name)
     end
   end
 
-  describe "scope tests" do
+  describe "scopes" do
     it "returns only active resource categories" do
-      active_category = ResourceCategory.create!(name: "Active Category", active: true)
-      inactive_category = ResourceCategory.create!(name: "Inactive Category", active: false)
-      expect(ResourceCategory.active).to include(active_category)
-      expect(ResourceCategory.active).not_to include(inactive_category)
+      res_cat1 = create(:resource_category, name: "res_cat1", active: true)
+      res_cat2 = create(:resource_category, name: "res_cat2", active: false)
+
+      expect(ResourceCategory.active).to include(res_cat1)
+      expect(ResourceCategory.active).not_to include(res_cat2)
     end
   end
 end
