84861e320020411b6593c24807903e8b0356e40a
Keenan Bromley <bromleyk@oregonstate.edu>
2025-02-10 12:58:54 -0800

    Created FactoryBot for resource_categories

--- spec/factories/resource_categories.rb
+++ spec/factories/resource_categories.rb
@@ -1,5 +1,10 @@
 FactoryBot .define do
   factory :resource_category do 
     name { "Resource Category" }
+    active { true }
+
+    trait :inactive do
+      active { false }
+    end
   end
 end
