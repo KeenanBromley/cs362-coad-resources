ad2e34df22132472e4ec45476cd097a93185aa8d
Keenan Bromley <bromleyk@oregonstate.edu>
2025-02-10 15:01:10 -0800

    Started on regions_controller_spec

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,6 +1,14 @@
 FactoryBot.define do
   factory :user do 
-    email { "User Email" }
-    role { "user role" }
+    email { "fake@fakeemail.com" }
+    password { "fakepassword" }
+
+    trait :admin do
+      role { :admin }
+    end
+
+    trait :organization do
+      role { :organization}
+    end 
   end
 end
