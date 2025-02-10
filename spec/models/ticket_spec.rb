90c2121f10491c2e9d48e63b2a42011280b141d3
Keenan Bromley <bromleyk@oregonstate.edu>
2025-02-10 13:20:30 -0800

    Updated ticket_spec to use FacotryBot

--- spec/models/ticket_spec.rb
+++ spec/models/ticket_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe Ticket, type: :model do
 
-  let (:ticket) {Ticket.new(id: 123)}
+  let (:ticket) {FactoryBot.build_stubbed(:ticket)}
   
   it "exists" do
     Ticket.new
