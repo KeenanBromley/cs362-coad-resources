11bc6f06990d94a5d85e552bc055a3ecd11b7ef7
Keenan Bromley <bromleyk@oregonstate.edu>
2025-02-13 22:14:01 -0800

    Refactored tests in region_spec

--- spec/models/region_spec.rb
+++ spec/models/region_spec.rb
@@ -3,43 +3,30 @@ require 'rails_helper'
 RSpec.describe Region, type: :model do
 
   let (:region) { FactoryBot.build_stubbed(:region) }
+  
   it "exists" do
     Region.new
   end
 
-  it "has a name" do
-    expect(region).to respond_to(:name)
+  describe "association" do
+    it { should have_many(:tickets) }
   end
 
-  it "has a string representation that is its name" do
-    name = 'Mt. Hood'
-    region = FactoryBot.build_stubbed(:region, name: 'Mt. Hood') 
-    result = region.to_s
-    expect(result).to eq('Mt. Hood')
-  end
-  it "has many tickets" do
-    should have_many(:tickets)
+  describe "validation" do
+    it { should validate_presence_of(:name) }
+    it { should validate_length_of(:name).is_at_least(1).is_at_most(255).on(:create) }
+    it { should validate_uniqueness_of(:name).case_insensitive }
   end
 
-  describe "validation tests" do
-    it "validates presence of name" do
-      should validate_presence_of(:name)
+  describe "instance methods" do
+    it "#self.unspecified" do
+      region = Region.unspecified
+      expect(region.name).to eq("Unspecified")
     end
 
-    it "validates length of name" do
-      should validate_length_of(:name).is_at_least(1).is_at_most(255).on(:create)
-    end
-
-    it "validates uniqueness of name" do
-      should validate_uniqueness_of(:name).case_insensitive
-    end 
-  end
-
-  describe "member functions tests" do
-    it "responds to to_s" do
-      expect(region).to respond_to(:to_s)
+    it "#to_s" do
+      expect(region.to_s).to eq(region.name)
     end
   end
-  
 
 end
