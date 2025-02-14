8c35e3fe7c2b76f11655652b269b388e49b7b174
Keenan Bromley <bromleyk@oregonstate.edu>
2025-02-13 22:03:20 -0800

    Refactored user_spec tests

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,65 +2,31 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
 
-  let (:user) {FactoryBot.build_stubbed(:user, email: "user@gmail.com", role: "admin")}
+  let (:user) {FactoryBot.build_stubbed(:user)}
   
   it "exists" do
     User.new
   end
 
-  it "responds to email" do
-    expect(user).to respond_to(:email)
+  describe "association" do
+    it { should belong_to(:organization).optional }
   end
 
-  it "responds to role" do
-    expect(user).to respond_to(:role)
+  describe "validation" do
+    it { validate_presence_of(:email) }
+    it { validate_length_of(:email).is_at_least(1).is_at_most(225).on(:create) }
+    it { validate_uniqueness_of(:email).case_insensitive }
+    it { validate_presence_of(:password).on(:create) }
+    it { validate_length_of(:password).is_at_least(7).is_at_most(255).on(:create) }
   end
 
-  it "belongs to organization" do
-    belong_to(:organization)
-  end
-
-  describe "validation tests" do
-    it "validates presence of email" do
-      should validate_presence_of(:email)
-    end 
-
-    it "validates length of email" do
-      should validate_length_of(:email).is_at_least(1).is_at_most(255).on(:create)
-    end 
-
-    it "validates uniqueness of email" do
-      should validate_uniqueness_of(:email).case_insensitive
-    end
-    
-    it "validates presence of password" do
-      should validate_presence_of(:password).on(:create)
-    end
-
-    it "validates length of password" do
-      should validate_length_of(:password).is_at_least(7).is_at_most(255).on(:create)
-    end 
-
-    valid_email = "bobthebuilder@gmail.com"
-    let (:user1) {FactoryBot.build_stubbed(:user, email: valid_email, role: "admin")}
-    invalid_email = "bobthebuilder"
-    let (:user2) {FactoryBot.build_stubbed(:user, email: invalid_email, role: "organization")}
-    it "validates email format" do 
-      valid_email = "bobthebuilder@gmail.com"
-      invalid_email = "bobthebuilder"
-      expect(user1).to be_valid
-      expect(user2).not_to be_valid
-    end
-  end 
-
-  describe "member function tests" do
-    it "responds to to_s" do
-      expect(user.to_s).to eq(user.email)
-    end 
-
-    it "responds to set_default role" do
+  describe "instance methods" do
+    it "#set_default_role" do
       expect(user.set_default_role).to eq(user.role)
     end
-  end
 
+    it "#to_s" do
+      expect(user.to_s).to eq(user.email)
+    end 
+  end
 end
