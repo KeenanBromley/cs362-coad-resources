828caf1f860721605f6423a69e5a4bf58b1b9520
Keenan Bromley <bromleyk@oregonstate.edu>
2025-02-24 14:49:30 -0800

    Added test to admin creates region spec

--- spec/features/regions/admin_creates_region_spec.rb
+++ spec/features/regions/admin_creates_region_spec.rb
@@ -2,7 +2,25 @@ require 'rails_helper'
 
 RSpec.describe 'Creating a Region', type: :feature do
 
+  before do
+    @admin = create(:user, :admin)
+  end
 
-log_in_as(admin)
+
+
+  it 'Admin creates a region spec' do
+    log_in_as(@admin)
+
+    visit root_path
+
+    click_on 'Regions'
+    click_on 'Add Region'
+
+    fill_in 'Name', with: "Collins house"
+    click_on 'Add Region'
+
+    expect(current_path).to eq regions_path
+
+  end
 
 end
