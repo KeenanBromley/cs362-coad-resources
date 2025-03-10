a2a9ebe1102a394772b10a44338cc561cefa5816
Colin Mant <colinm6666@gmail.com>
2025-03-10 15:51:45 -0700

    Finished approve_organization_spec.rb feature test

--- spec/features/organizations/approve_organization_spec.rb
+++ spec/features/organizations/approve_organization_spec.rb
@@ -1,5 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe 'Approving an organization', type: :feature do
+  before do
+    @admin = create(:user, :admin)
+    @organization = create(:organization)
+  end
 
+  it 'approves an organization' do
+    log_in_as(@admin)
+    visit root_path
+    click_on 'Organizations'
+    expect(current_path).to eq organizations_path
+
+    click_on 'Pending'
+    click_on 'Review'
+    click_on 'Approve'
+    expect(current_path).to eq organizations_path
+  end
 end
