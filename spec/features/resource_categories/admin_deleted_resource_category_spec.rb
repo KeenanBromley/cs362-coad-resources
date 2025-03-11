49ca97c65c47118f85050086c37408cdde4795c1
Keenan Bromley <bromleyk@oregonstate.edu>
2025-03-10 22:24:39 -0700

    Finished admin deleted res_cat spec

--- spec/features/resource_categories/admin_deleted_resource_category_spec.rb
+++ spec/features/resource_categories/admin_deleted_resource_category_spec.rb
@@ -1,5 +1,26 @@
 require 'rails_helper'
 
 RSpec.describe 'Deleting a Resource Category', type: :feature do
+  before do 
+    @admin = create(:user, :admin)
+  end
+  it 'Admin deletes a region spec' do
+    log_in_as(@admin)
+
+    res_cat = FactoryBot.create(:resource_category)
+
+    visit root_path
+
+    click_on 'Categories'
+
+
+    click_on res_cat.name
+    click_on 'Delete'
+
+    expect(current_path).to eq resource_categories_path
+
+    expect(page.body).to have_no_text('Animal Rescue')
+
+  end
 
 end
