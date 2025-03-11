81d67af6839137fe81c7a5f76b4b57aec192a0ae
Keenan Bromley <bromleyk@oregonstate.edu>
2025-03-11 13:52:34 -0700

    Added feature tests, TESTS DO NOT PASS

--- spec/features/users/user_login_spec.rb
+++ spec/features/users/user_login_spec.rb
@@ -1,5 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe 'Logging in', type: :feature do
+  it 'User Login' do
+    visit root_path
+    click_link 'Log in'
+    fill_in 'Email address', with: "bromleyk@oregonstate.edu"
+    fill_in 'Password', with: "password"
+    click_on 'Sign in'
+
+    expect(current_path).to eq dashboard_path
+  end
 
 end
--- spec/features/users/user_registration_spec.rb
+++ spec/features/users/user_registration_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe 'User registration', type: :feature do
+  it 'User registration' do
+    visit root_path
+    click_link 'Sign up'
+    fill_in 'Email address', with: "exampleemail@example.com"
+    fill_in 'Password', with: "password"
+    fill_in 'Password confirmation', with: "password"
 
+    click_on 'Sign up'
+  end
 end
