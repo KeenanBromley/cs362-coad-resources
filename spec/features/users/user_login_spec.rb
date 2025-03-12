148b1098e44c6b86760d3791cd2a3c5ab739a3ca
Keenan Bromley <bromleyk@oregonstate.edu>
2025-03-12 14:06:24 -0700

    Updated user_login_spec

--- spec/features/users/user_login_spec.rb
+++ spec/features/users/user_login_spec.rb
@@ -2,10 +2,11 @@ require 'rails_helper'
 
 RSpec.describe 'Logging in', type: :feature do
   it 'User Login' do
+    user = FactoryBot.create(:user)
     visit root_path
     click_link 'Log in'
-    fill_in 'Email address', with: "bromleyk@oregonstate.edu"
-    fill_in 'Password', with: "password"
+    fill_in 'Email address', with: user.email
+    fill_in 'Password', with: user.password
     click_on 'Sign in'
 
     expect(current_path).to eq dashboard_path
