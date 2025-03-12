35b03d9cfa86cc0d4d6b84b0e13562345da73771
Colin Mant <colinm6666@gmail.com>
2025-03-12 14:12:37 -0700

    reject_organization_spec.rb is passing

--- spec/features/organizations/reject_organization_spec.rb
+++ spec/features/organizations/reject_organization_spec.rb
@@ -1,5 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe 'Rejecting an organization', type: :feature do
+  before do
+    @admin = create(:user, :admin)
+    @organization = create(:organization)
+  end
 
+  it 'rejects an organization' do
+    log_in_as(@admin)
+    visit root_path
+    click_on 'Organizations'
+    expect(current_path).to eq organizations_path
+
+    click_on 'Pending'
+    click_on 'Review'
+    click_on 'Reject'
+    expect(current_path).to eq organizations_path
+  end
 end
