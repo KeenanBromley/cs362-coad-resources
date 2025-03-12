5bf6dd2761687bfd64966fc33fdf99ef7e8f8b3f
Keenan Bromley <bromleyk@oregonstate.edu>
2025-03-12 15:35:01 -0700

    Finished capture_ticket_spec

--- spec/features/tickets/capture_ticket_spec.rb
+++ spec/features/tickets/capture_ticket_spec.rb
@@ -1,5 +1,22 @@
 require 'rails_helper'
 
 RSpec.describe 'Capturing a ticket', type: :feature do
+  before do
+    @user = create(:user, :organization_approved)
+    
+  end 
+
+  it' captures the ticket' do
+    ticket = FactoryBot.create(:ticket, name: 'Help Me')
+    log_in_as(@user)
+    visit dashboard_path
+    click_on ticket.name
+    pp ticket
+    click_on 'Capture'
+    ticket2 = Ticket.find(ticket.id)
+
+    expect(current_path).to eq dashboard_path
+    expect(ticket2.organization_id).to eq(@user.organization.id)
+  end
 
 end
